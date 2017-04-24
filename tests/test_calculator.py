a0bcf038a12039a7c67e007f415dd4582a52bd21
Brynjar Smári <binni_s@hotmail.com>
2017-04-24 14:07:23 +0000

    Updated test_calculator.py

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -4,5 +4,5 @@ def test_add():
     assert Calc.Calculator.add(1,1) == 2
 def test_pass():
     assert True == True
-def test_fail():
-    assert True == False
+def test_pass2():
+    assert False == False
