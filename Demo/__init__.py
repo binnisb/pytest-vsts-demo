d42b5c38c7783dcacafcae1e8070768ccb872f43
Brynjar Smári <binni_s@hotmail.com>
2018-03-18 06:15:37 +0000

    Updated __init__.py

--- Demo/__init__.py
+++ Demo/__init__.py
@@ -13,16 +13,3 @@ def __cli_add():
     res = add(x, y)
     print(res)
     return 0
-
-def __stock_fetch():
-    """
-    Usage:
-        stock_fetch <stock> <start_date> <end_date>
-    """
-    parse_date = lambda date_str: datetime.datetime.strptime(date_str,"%Y-%m-%d").date()
-    args = docopt(__stock_fetch.__doc__)
-    stock, start_date, end_date = args["<stock>"], parse_date(args["<start_date>"]), parse_date(args["<end_date>"])
-
-    df = read_stock(stock,start_date,end_date)
-    df.to_csv("./{stock}-{start}-{end}.csv".format(stock=stock,start=start_date,end=end_date))
-    print(df.to_csv())
